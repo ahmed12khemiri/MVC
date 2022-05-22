34b21c690a56d7f3c36e0b1e99d2041182228dcd
ahmed12khemiri <ahmedkhemiri123456@gmail.com>
2022-05-22 15:31:01 +0200

    Initial commit

---
+++ src/controllers/AuthentificationServlet.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.AuthentificationBean;
+import beans.controllerBean;
+
+/**
+ * Servlet implementation class AuthentificationServlet
+ */
+@WebServlet("/AuthentificationServlet")
+public class AuthentificationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Object Cb = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuthentificationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nom = request.getParameter("name");
+		String prenom = request.getParameter("Uname");
+		
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("Cb")==null) {
+			AuthentificationBean Cb = new AuthentificationBean();
+			Cb.setNom(nom);
+			Cb.setPrenom(prenom);
+			session.setAttribute("Cb", Cb);
+		}
+		else {
+			
+		}
+		
+		
+		
+		String adress = "Authentification.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(adress);
+		dispatcher.forward(request, response);
+	}
+
+}
